a8904988c4bf9c11782e5182567a4a8fb134124d
Arjun T A <arjunta32@gmail.com>
2025-04-26 19:50:16 +0530

    ascii art added

--- hangman.py
+++ hangman.py
@@ -1,6 +1,9 @@
 import random
+import hangman_Art
+
 words = ["cat", "rat", "dog", "lion", "tiger", "elephant", "giraffe", "zebra", "bear", "wolf"]
 placeholder=""
+lives=6
 animal=words[random.randint(0,len(words)-1)]
 print(animal)
 
@@ -13,12 +16,25 @@ chances= len(animal)
 
 correct_letters=[]
 
-while(chances):
+gameOver= False
+
+
+while not gameOver:
+    print(hangman_Art.stages[lives-1])
+    
     guess= input("Take a guess: ").lower()
 
+    
+    
+    if guess not in animal:
+        lives-=1
+    
     chances-=1
     
     display=""
+    
+    
+
 
     for i in range(len(animal)):
         if animal[i]==guess:
@@ -28,5 +44,13 @@ while(chances):
             display+="{} ".format(animal[i])
         else:
             display+="_ "
+    
+    if '_' not in display:
+        print("YOU WIN")
+        gameOver=True
+    elif lives==0:
+        print("YOU LOOSE")
+        gameOver=True
+
 
     print(display+"\n\n\n")
