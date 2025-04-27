77d2b22695043a3fe42037140be633b3940fbca6
Arjun T A <arjunta32@gmail.com>
2025-04-27 17:21:08 +0530

    calc project

---
+++ calculator.py
@@ -0,0 +1,67 @@
+import calc_art
+print(calc_art.image)
+
+def asknum():
+    try:
+        num1= int(input("\n\n WHATS THE FIRST NUMBER?: "))
+        return num1
+    except:
+        print("Enter number only")
+        return asknum()
+
+def asknum2():
+    try:
+        num2= int(input("\n\n WHATS THE NEXT NUMBER?: "))
+        return num2
+    except:
+        print("Enter number only")
+        return asknum2()
+
+
+def doOperation(n1,n2,op):
+    if op=="+":
+        return n1+n2
+    elif op=="*":
+        return n1*n2
+    elif op=="/":
+        return n1/n2
+    elif op=="-":
+        return n1-n2
+
+
+def main():
+    num1= asknum()
+    ans=0
+    
+    def calc(num):
+        global ans
+        operation= input("+\n-\n*\n/\nPick an operation:")
+        if(operation not in "+-/*"):
+            print("wrong input")
+            return
+        num2= asknum2()        
+        ans= doOperation(num,num2,operation)        
+        print(f"{num} {operation} {num2} = {ans}")
+        
+    
+    calc(num1)
+    res= input(f"Type 'y' to continue calculting with {ans} or 'n' to start new calc")
+
+    if res=="n":
+        main()
+    elif res=="y":
+        calc(ans)
+        # operation= input("+\n-\n*\n/\nPick an operation:")
+        # if(operation not in "+-/*"):
+        #     print("wrong input")
+        #     return
+        
+        # num2= asknum2()
+        
+        # newAns= doOperation(ans,num2,operation)
+        # print(f"{ans} {operation} {num2} = {newAns}")
+    else:
+        print("THANKYOU FOR USING")
+        return     
+
+main()
