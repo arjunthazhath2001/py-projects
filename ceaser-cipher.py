87ecfe123a771ccd4a911a58099560c731c12183
Arjun T A <arjunta32@gmail.com>
2025-04-27 14:03:20 +0530

    completed ceaser-cipher

---
+++ ceaser-cipher.py
@@ -0,0 +1,42 @@
+
+import cipher_art
+
+print(cipher_art.title)
+
+def encode(text,shifts):
+    encoded=""
+    for t in text:
+        index=cipher_art.letters.index(t)
+        encoded += cipher_art.letters[index+shifts]
+    print("\n\n Decoded Text: "+ encoded)    
+
+
+def decode(text,shifts):
+    decoded=""
+    for t in text:
+        index=cipher_art.letters.index(t)
+        decoded+= cipher_art.letters[index-shifts]
+    print("\n\n Encoded Text: "+decoded)    
+    
+
+
+def encode_decode():
+    decision= input("\n\n Do you want to encode or decode? Press 'e' or 'd': ")
+    text= input("\n\n Enter the text: ")
+    shifts= int(input("\n\n What is the number of shifts?: "))
+    
+    if decision=="e":
+        encode(text,shifts)      
+    elif decision=="d":
+        decode(text,shifts)        
+    else:
+        print("Wrong input")
+
+    response= input("Do u wish to continue? y or n")
+    if response=="y":
+        encode_decode()
+    else:
+        return
+
+
+encode_decode()
