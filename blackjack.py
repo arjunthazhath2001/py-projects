7d29915eeed0146599f0cc1f825a614a9485886d
Arjun T A <arjunta32@gmail.com>
2025-04-27 19:26:40 +0530

    generating 2 random cards for dealer and player

--- blackjack.py
+++ blackjack.py
@@ -2,4 +2,21 @@ import random
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
+user_card1= random.randint(0,len(cards)-1)
+user_card2= random.randint(0,len(cards)-1)
 
+
+comp_card1= random.randint(0,len(cards)-1)
+comp_card2= random.randint(0,len(cards)-1)
+
+
+user_cards= [cards[user_card1],cards[user_card2]]
+comp_cards= [cards[comp_card1], cards[comp_card2]]
+
+print(f"Your cards:  {user_cards}")
+print(f"Computer's first card: {comp_cards[0]}")
+
+
+
+for i in range(len(user_cards)):
+    if     
