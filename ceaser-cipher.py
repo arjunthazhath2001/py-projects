a61ed2053534d8011d1041649cdede08d8fdb718
Arjun T A <arjunta32@gmail.com>
2025-04-27 14:21:45 +0530

    fixed bugs in ceaser-cipher

--- ceaser-cipher.py
+++ ceaser-cipher.py
@@ -6,25 +6,35 @@ print(cipher_art.title)
 def encode(text,shifts):
     encoded=""
     for t in text:
+        if t not in cipher_art.letters:
+            decoded+=t
+            
         index=cipher_art.letters.index(t)
-        encoded += cipher_art.letters[index+shifts]
+        newShift= (index+shifts)%52
+        encoded += cipher_art.letters[newShift]
     print("\n\n Decoded Text: "+ encoded)    
 
 
 def decode(text,shifts):
     decoded=""
     for t in text:
+        if t not in cipher_art.letters:
+            decoded+=t
         index=cipher_art.letters.index(t)
-        decoded+= cipher_art.letters[index-shifts]
+        newShift= (index+shifts)%52
+        decoded+= cipher_art.letters[newShift]
     print("\n\n Encoded Text: "+decoded)    
     
 
 
 def encode_decode():
     decision= input("\n\n Do you want to encode or decode? Press 'e' or 'd': ")
+    if decision not in "ed":
+        print("Wrong input")
+        return
     text= input("\n\n Enter the text: ")
     shifts= int(input("\n\n What is the number of shifts?: "))
-    
+
     if decision=="e":
         encode(text,shifts)      
     elif decision=="d":
