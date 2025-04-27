55724820e5ce99f01550c0893f47e41b1c14f6ba
Arjun T A <arjunta32@gmail.com>
2025-04-27 15:18:20 +0530

    completed secret auction program game

---
+++ auction.py
@@ -0,0 +1,27 @@
+import auction_art
+import os
+
+bidList={}
+
+print(auction_art.hammer)
+
+print("WELCOME TO SECRET AUCTION PROGRAM")
+
+def bidding():
+    name= input("\n\n WHATS UR NAME?: ")
+    bid= int(input("\n\n WHATS YOUR BID?: "))
+    bidList[name]=bid
+    response= input("\n\n ARE THERE ANY OTHER BIDDERS. Type y or n.: ")
+    if response=="y":
+        os.system('clear')
+        bidding()
+    else:
+        maxBid= max(bidList.values())
+        for key in bidList:
+            if bidList[key]==maxBid:
+                print("************************************************")
+                print(f"\n\nThe winner is {key} with a bid of {maxBid}\n\n")
+                print("************************************************")
+        return
+
+bidding()
