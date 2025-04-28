d45520c476f9a770184d4e108269b58d7c06a025
Arjun T A <arjunta32@gmail.com>
2025-04-28 18:46:27 +0530

    finished the game

--- guess_a_num.py
+++ guess_a_num.py
@@ -2,29 +2,34 @@ import random
 from guess_art import *
 
 print(logo)
-print("Welcome to the Number Guessing Game!\n I'm thinking of a number between 1 and 100.")
+print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
 
 easy_attempts=10
-
+hard_attempts=5
 num= random.randint(1,100)
 
-difficulty= input("Choose a difficulty. Type 'easy' or 'hard': ")
 
-if difficulty=="easy":
-    print(f"You have {easy_attempts} attempts remaining to guess the number.")
-    user_num= input("Make a guess")
-    while easy_attempts>0:
-        if user_num<num:
+
+def game(attempts):
+    for i in range(attempts,0,-1):
+        print(f"You have {i} remaining")
+        guess= int(input("Take a guess: "))
+        
+        if guess==num:
+            print(f"You are right. The number is {num}")
+            break
+        elif guess<num:
             print("Too low")
-            print("Guess Again")
-            attempts-=1
-            print(f"You have {easy_attempts} attempts remaining to guess the number.")
-        elif user_num>num:
-            print("Too high")
-            print("Guess Again")
-            easy_attempts-=1
-            print(f"You have {easy_attempts} attempts remaining to guess the number.")
+            continue
         else:
-            print(f"You got it. The answer was {num}")
-    else:
-        print("You've run out of guesses.")
+            print("Too high")
+            continue
+
+
+difficulty= input("Choose a difficulty. Type 'easy' or 'hard': ")
+if(difficulty=="easy"):
+    game(easy_attempts)
+elif(difficulty=="hard"):
+    game(hard_attempts)
+else:
+    print("Please enter correct value")
