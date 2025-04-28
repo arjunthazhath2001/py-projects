c1c660baf48c95d79300d1fd93feab09b90f24ba
Arjun T A <arjunta32@gmail.com>
2025-04-28 18:29:41 +0530

    easy feature

--- guess_a_num.py
+++ guess_a_num.py
@@ -2,12 +2,29 @@ import random
 from guess_art import *
 
 print(logo)
-attempts=10
 print("Welcome to the Number Guessing Game!\n I'm thinking of a number between 1 and 100.")
+
+easy_attempts=10
 
 num= random.randint(1,100)
 
 difficulty= input("Choose a difficulty. Type 'easy' or 'hard': ")
 
 if difficulty=="easy":
-    print(f"You have {attempts} attempts remaining to guess the number.")
+    print(f"You have {easy_attempts} attempts remaining to guess the number.")
+    user_num= input("Make a guess")
+    while easy_attempts>0:
+        if user_num<num:
+            print("Too low")
+            print("Guess Again")
+            attempts-=1
+            print(f"You have {easy_attempts} attempts remaining to guess the number.")
+        elif user_num>num:
+            print("Too high")
+            print("Guess Again")
+            easy_attempts-=1
+            print(f"You have {easy_attempts} attempts remaining to guess the number.")
+        else:
+            print(f"You got it. The answer was {num}")
+    else:
+        print("You've run out of guesses.")
