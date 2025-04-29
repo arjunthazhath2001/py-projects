c907e9ea9b705b1cb21f6ef0eeb6f3ba933d2c53
Arjun T A <arjunta32@gmail.com>
2025-04-29 19:37:12 +0530

    completed

--- games/oops/main.py
+++ games/oops/main.py
@@ -4,6 +4,25 @@ from money_machine import MoneyMachine
 
 
 
-items=Menu()
+menu=Menu()
+coffee= CoffeeMaker()
+money= MoneyMachine()
 
-print(items.get_items())
+is_on= True
+
+
+while is_on:
+
+    order= input(f"Whats your choice: ({menu.get_items()})")
+    if order=="off":
+        is_on=False
+    elif order=="report":
+        coffee.report()
+        money.report()
+    else:
+        drink= menu.find_drink(order)
+        if coffee.is_resource_sufficient(drink):
+            if money.make_payment(drink.cost):
+                coffee.make_coffee(drink)
+            
+
