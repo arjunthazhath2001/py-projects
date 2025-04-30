5ed18dd08a276bcf6d51193945226f9e988a54d9
Arjun T A <arjunta32@gmail.com>
2025-04-30 20:21:40 +0530

    extracted colors

---
+++ games/turtle/spot.py
@@ -0,0 +1,30 @@
+# import colorgram
+# colors = colorgram.extract('images.jpeg', 20)
+
+# result=[]
+
+# # for i in range(0,20):
+# #     first_color = colors[i]
+# #     rgb = first_color.rgb
+# #     red = rgb[0]
+# #     green= rgb[1]
+# #     blue= rgb[2]
+# #     result.append((red,green,blue))
+
+colors= [
+    (177, 173, 8),
+    (239, 57, 213),
+    (36, 180, 10),
+    (158, 4, 90),
+    (235, 48, 10),
+    (212, 8, 4),
+    (59, 145, 241),
+    (67, 4, 146),
+    (244, 53, 17),
+    (221, 138, 63),
+    (234, 214, 2),
+    (216, 121, 200),
+    (172, 52, 108)
+]
+
+
