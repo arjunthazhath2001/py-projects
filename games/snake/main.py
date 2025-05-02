0418a7e92db93757142d10978d1a633574511f8f
arjunthazath <arjunta32@gmail.com>
2025-05-02 15:31:41 +0530

    moving feature

--- games/snake/main.py
+++ games/snake/main.py
@@ -7,11 +7,36 @@ screen.title('Snake game')
 
 starting_pos= [(0,0),(20,0),(-20,0)]
 
+# screen.tracer(0)
+segments=[]
 
 for pos in starting_pos:
     new_seg= Turtle("square")
     new_seg.color("white")
+    new_seg.penup()
     new_seg.goto(pos)
+    segments.append(new_seg)
+
+game_is_on=True
+
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
+        screen.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 screen.exitonclick()
