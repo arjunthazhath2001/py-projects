334e27b8da0ce8ee233cadc81bc91ced4b220414
Arjun T A <arjunta32@gmail.com>
2025-05-03 19:17:42 +0530

    pandas and pandas

--- files/weather/main.py
+++ files/weather/main.py
@@ -16,21 +16,32 @@ data= pandas.read_csv("weather_data.csv")
 # temp= data[1].to_list()
 # print(temp)
 
-print(type(data)) #DATA FRAME
-print(type(data["temp"])) # SERIES
+# print(type(data)) #DATA FRAME
+# print(type(data["temp"])) # SERIES
 
-avg_temp=data["temp"].max()
-max_temp=data["temp"].max()
+# avg_temp=data["temp"].max()
+# max_temp=data["temp"].max()
 
 
-print(avg_temp)
+# print(avg_temp)
     
     
-print(data.condition)
+# print(data.condition)
 
-print(data[data.temp==max_temp])
+# print(data[data.temp==max_temp])
 
 
-monday= data[data.day=="Monday"]
+# monday= data[data.day=="Monday"]
 
-print((monday["temp"]*1.8) +32)
+# print((monday["temp"]*1.8) +32)
+
+
+data_dict= {
+    "students": ["arjun","anil","rahul"],
+    "marks": [90,45,45]
+}
+
+data=pandas.DataFrame(data_dict)
+
+# print(data)
+data.to_csv("new_data.csv")
