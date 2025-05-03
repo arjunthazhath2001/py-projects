82bbf30f3b1a88ba922bde5afa80ddfd06450516
Arjun T A <arjunta32@gmail.com>
2025-05-03 20:53:29 +0530

    learning df

--- files/squirrel/main.py
+++ files/squirrel/main.py
@@ -4,10 +4,27 @@ import pandas
 data= pandas.read_csv("sq_data.csv")
 
 
-# colors= data["Primary Fur Color"].value_counts()
 
+
+gray=len(data[data["Primary Fur Color"]=="Gray"])
+cina=len(data[data["Primary Fur Color"]=="Cinnamon"])
+black=len(data[data["Primary Fur Color"]=="Black"])
+
+
+
+data_dict={
+    "Fur Color": ["Gray","Cinnamon","Black"],
+    "Count": [gray,cina,black]
+}
+
+
+newdata= pandas.DataFrame(data_dict)
+newdata.to_csv('trial.csv')
+
+
+# colors= data["Primary Fur Color"].value_counts()
 # data=pandas.DataFrame(colors)
 
 
 # print(data)
-data.to_csv("sq_fur_data.csv")
+# data.to_csv("sq_fur_data.csv")
