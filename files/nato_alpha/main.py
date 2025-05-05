4ee70b21379e054bb93c7be0d45431f6d7e9c520
Arjun T A <arjunta32@gmail.com>
2025-05-05 14:54:30 +0530

    new error handling updates

--- files/nato_alpha/main.py
+++ files/nato_alpha/main.py
@@ -5,10 +5,14 @@ data= pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row["letter"]: row["code"] for (index, row) in data.iterrows()}
 
+game_on=True
+while game_on:
+    word= list(input("Enter a name").upper())
 
-
-word= list(input("Enter a name").upper())
-
-res= [nato_dict[w] for w in word]
-
-print(res)
+    try:
+        res= [nato_dict[w] for w in word]
+    except KeyError:
+        print("only alpha please")
+        
+    else:
+        print(res)
