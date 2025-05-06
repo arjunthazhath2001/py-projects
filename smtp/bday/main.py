611120b2ce1dd1051b521526f8ccf4e2e14566e7
Arjun T A <arjunta32@gmail.com>
2025-05-06 13:06:42 +0530

    completed birthday mail sender

--- smtp/bday/main.py
+++ smtp/bday/main.py
@@ -1,53 +1,56 @@
 import random
 import pandas
-import datetime as dt
+from datetime import datetime
+import smtplib
 
 
-data= pandas.read_csv('dob.csv')
-
-data=data.to_dict()
-
-# name=data['name'].to_list()
-# mail= data['mail'].to_list()
-# dob= data['dob'].to_list()
+my_email= "arjunta000@gmail.com"
+password="chav mqgd qcvc zons"
 
 
-# for d in dob:
-#     d.split('-')
+data= pandas.read_csv('birthdays.csv')
 
 
-# letter_num= random.randint(1,3)
-# with open(f"./letter_{letter_num}.txt") as file:
-#     contents= file.read()
+
+people=[]
+letters=[]
+mails=[]
+
+def send_mail(letters):
+    for i in range(len(letters)):
+        with open(f"{letters[i]}") as file:
+            mail= file.read()
+        
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+
+            connection.login(user=my_email,password=password)
+
+            connection.sendmail(from_addr=my_email, to_addrs=f"{mails[i]}", msg=f"Subject:HAPPY BIRTHDAY\n\n{mail}")
+
+        
+def create_mail(names):  
+    letter_number= random.randint(1,3)
+
+    with open(f"./letter_{letter_number}.txt") as file:
+        contents= file.read()
+        
+        
+    for i in range(len(names)):
+        new_letter= contents.replace('[name]',names[i])
+        letters.append(f"./letter_for_{names[i]}.txt")
+        with open(f"./letter_for_{names[i]}.txt",'w') as file:
+            file.write(new_letter)
     
-
-current_day= dt.datetime.now().day
-current_month= dt.datetime.now().month
-
-# now= dt.datetime.now()
-print(current_day)
-print(current_month)
-
-    
-# bday_person= 
+    send_mail(letters)
 
 
-# with open("./input/names/invited_names.txt") as file:
-#     p_names=file.readlines()
-    
-
-# print(p_names)
-
-# for i in range(len(p_names)):
-#     stripped_name=p_names[i].strip('\n')
-#     new_letter= contents.replace('[name]',stripped_name) 
-#     with open(f"./output/readytosend/letter_for_{stripped_name}.docx",'w') as file:
-#         file.write(new_letter)
+for index, row in data.iterrows():
+    if row["month"] == datetime.now().month and row["day"] == datetime.now().day:
+        people.append(row["name"])
+        mails.append(row["email"])            
 
 
-
-
-
-# print(name)
-# print(mail)
-# print(dob)
+if people:
+    create_mail(people)
+            
