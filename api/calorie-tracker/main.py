bf4a515404e62c5a307d4f2fb53d425ff68ca696
Arjun T A <arjunta32@gmail.com>
2025-05-08 13:31:54 +0530

    completed calories tracker for exercises

--- api/calorie-tracker/main.py
+++ api/calorie-tracker/main.py
@@ -2,11 +2,15 @@ import requests
 import os
 from dotenv import load_dotenv
 
+import datetime as dt
+
 load_dotenv()
 
 
 APP_ID=os.getenv('APP_ID')
 API_KEY=os.getenv('API_KEY')
+
+AUTH_HEADER=os.getenv('AUTH_HEADER')
 
 USER_NAME="da71f394d813c70dbdef5466e123c1eb"
 PROJECT_NAME="myWorkouts"
@@ -28,32 +32,44 @@ headers={
 
 data=requests.post(url=nat_lang_endpoint,json=query_params, headers=headers)
 
-print(data.text)
+data=data.json()
+
+res=[]
+
+
+for i in range(len(data["exercises"])):
+    res.append([data["exercises"][i]['name'],data["exercises"][i]['duration_min'],data["exercises"][i]['nf_calories']])
+
+print(res)
 
 
 SHEETY_URL="https://api.sheety.co/da71f394d813c70dbdef5466e123c1eb/myWorkouts/workouts"
 
 headers = {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "Authorization": AUTH_HEADER
 }
 
 
 
+date=dt.datetime.now().strftime("%d/%m/%Y")
+time=dt.datetime.now().strftime("%H:%M:%S")
 
 
-sheet_params={
-    "workout":{
+for i in range(len(res)):
+    sheet_params={
+        "workout":{
 
-        "date":"08/04/2025",
-        "time": "03:00:00",
-        "exercise": "Running",
-        "duration": "22",
-        "calories": "20"
-        
+            "date": date,
+            "time": time,
+            "exercise": res[i][0],
+            "duration": str(res[i][1]),
+            "calories": str(res[i][2])
+            
+        }
     }
-}
 
 
 
-response= requests.post(url=SHEETY_URL,json=sheet_params,headers=headers)
-print(response.text)
+    response= requests.post(url=SHEETY_URL,json=sheet_params,headers=headers)
+    print(response.text)
