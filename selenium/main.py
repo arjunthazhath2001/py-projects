125c791571e90e9a86728fa7f1488be6dac9cb9f
Arjun T A <arjunta32@gmail.com>
2025-05-10 13:52:15 +0530

    getting elements data using selenium

--- selenium/main.py
+++ selenium/main.py
@@ -12,7 +12,21 @@ driver.get("https://www.python.org/")
 
 
 search_bar=driver.find_element(By.NAME,value="q")
-print(search_bar.tag_name)
+print(search_bar.get_attribute("placeholder"))
+
+
+go_button= driver.find_element(By.ID,value="submit")
+print(go_button.size)
+
+
+# documentation_link= driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
+
+documentation_link= driver.find_element(By.CSS_SELECTOR, value=".small-widget.documentation-widget a") 
+print(documentation_link.text)
+
+bug_link= driver.find_element(By.XPATH,value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+
+print(bug_link.text)
 
 # driver.close()
 driver.quit()
